f97b289aa19e08e87176776b29e9704b51d4da63
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2024-06-04 19:31:13 +0300

    added testing.cmake, compilers.cmake -> defaults.cmake

--- helpers.cmake
+++ helpers.cmake
@@ -1,2 +1,3 @@
-include(${CMAKE_CURRENT_LIST_DIR}/compilers.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/defaults.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/testing.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/install.cmake)
