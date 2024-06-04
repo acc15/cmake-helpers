2397ac4c33b2640ae69a2ced4b5a42d20fdb1042
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2024-06-04 19:31:57 +0300

    added testing.cmake, compilers.cmake -> defaults.cmake

---
+++ defaults.cmake
@@ -0,0 +1,37 @@
+
+macro(configure_defaults)
+
+    cmake_parse_arguments(DEFAULTS "WITH_SHARED_LIBS" "" "" "${ARGV}")
+
+    set(CMAKE_CXX_STANDARD 20)
+    set(CMAKE_CXX_STANDARD_REQUIRED ON)
+    set(CMAKE_DEBUG_POSTFIX "d")
+    
+    if(MSVC)
+        add_compile_options(
+            /W4 # Enable informational warnings (1-4 level)
+            /WX # Treat warnings as errors
+            /utf-8 # Enable UTF-8 support
+            /Zc:wchar_t # wchar_t as builtin type
+            /Zc:forScope # Removes possibility to use out of scope loop variables
+        
+            # Removes unreferenced data or functions that are COMDATs, or that only have internal linkage. 
+            # Greatly reduces binary size
+            /Zc:inline
+        )
+    else()
+        add_compile_options(-Wall -Wextra -Wpedantic)
+    endif()
+
+    # shared libraries
+    if(DEFAULTS_WITH_SHARED_LIBS)
+        option(BUILD_SHARED_LIBS "Build using shared libraries" ON)
+        option(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS "Exports all symbols from library (Windows only)" ON)
+    endif()
+
+    # standard directory path prefixes
+    set(SRC src)
+    set(INC include/${PROJECT_NAME})
+    set(TEST test)
+
+endmacro()
---
+++ testing.cmake
@@ -0,0 +1,31 @@
+function(testing)
+    
+    cmake_parse_arguments(PARSE_ARGV 0 TESTING "CUSTOM_MAIN" "NAME;DIR" "TARGETS;SRC")
+    if(NOT TESTING_NAME)
+        set(TESTING_NAME ${PROJECT_NAME}_test)
+    endif()
+    if(NOT TESTING_DIR)
+        set(TESTING_DIR test)
+    endif()
+
+    include(CTest)
+    if(BUILD_TESTING)
+
+        set(TESTING_LIBS ${TESTING_TARGETS})
+        if(TESTING_CUSTOM_MAIN)
+            list(PREPEND TESTING_LIBS Catch2::Catch2)
+        else()
+            list(PREPEND TESTING_LIBS Catch2::Catch2WithMain)
+        endif()
+
+        find_package(Catch2 REQUIRED)
+        add_executable(${TESTING_NAME} ${TESTING_SRC})
+        target_include_directories(${TESTING_NAME} PRIVATE ${TESTING_DIR})
+        target_link_libraries(${TESTING_NAME} PRIVATE ${TESTING_LIBS})
+
+        include(Catch)
+        catch_discover_tests(${TESTING_NAME})
+
+    endif()
+
+endfunction()
